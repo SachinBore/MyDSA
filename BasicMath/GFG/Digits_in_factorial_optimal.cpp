92bf354f218ff6dfcba8c193aee8f34efefd0daf
Sachin <58112222+SachinBore@users.noreply.github.com>
2023-06-01 21:35:32 +0530

    Create Digits_in_factorial_optimal.cpp

---
+++ BasicMath/GFG/Digits_in_factorial_optimal.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int solve(int n) {
+
+	double res = 0;
+	for (int i = 1; i <= n; i++) {
+		res += log10(i);
+	}
+	res = floor(res) + 1;
+	return res;
+}
+
+
+int main() {
+#ifndef ONLINE_JUDGE
+	freopen("inputmath.txt", "r", stdin);
+	freopen("outputmath.txt", "w", stdout);
+#endif
+
+	int testcase; cin >> testcase;
+	while (testcase--) {
+		int n; cin >> n;
+		cout << solve(n) << endl;;
+	}
+
+}
